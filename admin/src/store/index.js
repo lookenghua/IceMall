8a02b202cc9d41835ec110385426eafe4dde6df6
lookenghua <lookenghua@gmail.com>
2022-08-13 21:46:13 +0800

    chore: 增加pinia插件colada

--- admin/src/store/index.js
+++ admin/src/store/index.js
@@ -1,9 +1,12 @@
 import { createPinia } from "pinia";
-
+import Colada, { PiniaColadaPlugin } from "colada-plugin";
 const store = createPinia();
 
 export function setupStore(app) {
   app.use(store);
+  // 时间穿梭机
+  store.use(PiniaColadaPlugin);
+  app.use(Colada);
 }
 
 export { store };
